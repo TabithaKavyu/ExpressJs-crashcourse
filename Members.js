0fa8d2052521390085ed5e52fabfbe33da6e9663
tabit <tabithakavyu4@gmail.com>
2020-03-20 23:10:16 +0300

    initial commit

---
+++ Members.js
@@ -0,0 +1,23 @@
+const members = [
+    {
+       id: 1,
+      name: "Leanne Graham",
+      email: "Sincere@april.biz",
+      status: 'active'
+   },
+   {
+       id: 2,
+       name: "Ervin Howell",
+       email: "Shanna@melissa.tv",
+       status: 'inactive'
+   },
+   {
+       id: 3,
+       name: "Patricia Lebsack",
+       email: "Julianne.OConner@kory.org",
+       status: 'active'
+   }
+   ];
+
+   module.exports = members;
+
